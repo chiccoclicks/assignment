13eda79cdd933b16247f6605b7a43ee79da8255a
chiccoclicks <95689429+chiccoclicks@users.noreply.github.com>
2022-04-12 23:47:07 +0200

    Update appy.py

--- appy.py
+++ appy.py
@@ -11,14 +11,14 @@ import PIL
 model= load_model('classify.hdf5',compile=(False))
 
 #body and functions of the app
-def creating_frames(name):
+def video_splitter(name):
     vidcap = cv2.VideoCapture(name)
     success,frame = vidcap.read()
     count = 0
     frame_skip =1
     while success:
         success, frame = vidcap.read() # get next frame
-        cv2.imwrite(r"C:\Users\zhoub\Desktop\test\final_project\frame%d.jpg" % count, frame) 
+        cv2.imwrite(r"C:\Users\Mushati\Desktop\ass\frame%d.jpg" % count, frame) 
         if count % frame_skip == 0: # analyse frames
             print('frame: {}'.format(count)) 
             pil_img = Image.fromarray(frame) # convert frames
@@ -30,24 +30,24 @@ def creating_frames(name):
     converting_vidd()
 
 def converting_vidd():
-    idx = tf.io.read_file('frame4.jpg')
-    idx = tf.io.decode_image(idx,channels=3) 
-    idx = tf.image.resize(idx,[299,299])
-    idx = tf.expand_dims(idx, axis=0)
-    idx = tf.keras.applications.inception_v3.preprocess_input(idx)
-    return idx
+    x = tf.io.read_file('frame4.jpg')
+    x = tf.io.decode_image(x,channels=3) 
+    x = tf.image.resize(x,[299,299])
+    x = tf.expand_dims(x, axis=0)
+    x = tf.keras.applications.inception_v3.preprocess_input(x)
+    return x
     
-def predict(idx):
-    mod_pred = tf.keras.applications.inception_v3.decode_predictions(model.predict(idx), top=1)
+def predict(x):
+    mod_pred = tf.keras.applications.inception_v3.decode_predictions(model.predict(x), top=1)
     return mod_pred
     
 def main():
-    st.sidebar.title("OBJECT RECOGNITION.")
-    st.title("INCEPTIONV3")
+    st.title("computer_vision.")
+    
     vid = None
 
-    Search = st.sidebar.text_input("Search for an object",)
-    uploaded_video = st.sidebar.file_uploader("Choose Video",type=(['avi','mp4','mov','mkv']))
+    Search = st.text_input("Search for an object",)
+    uploaded_video = st.file_uploader("Choose Video",type=(['avi','mp4']))
     if uploaded_video is not None: 
         vid = uploaded_video.name
         with open(vid, mode='wb') as f:
@@ -63,8 +63,8 @@ def main():
         cur_frame = 0
         success = True
         
-    if st.sidebar.button("RECOGNISE"):
-        convert_frames = creating_frames(vid)
+    if st.sidebar.button("Detect"):
+        convert_frames = video_splitter(vid)
         output = converting_vidd()
         results_final = predict(output)
     
